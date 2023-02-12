1f562ddce08cd0c75fd68572188d1c581105c565
Pratik Roshan Shah <pratik.roshan.21@gmail.com>
2023-02-12 21:36:20 +0545

    Models and DataBases

--- Second_Project/myapp/models.py
+++ Second_Project/myapp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Topic(models.Model):
     top_name = models.CharField(max_length=264, unique=True)
+
+    class Meta:
+        app_label = 'myapp'
 
     def __str__(self):
         return self.top_name
@@ -13,6 +17,9 @@ class Webpage(models.Model):
     name = models.CharField(max_length=264, unique=True)
     url = models.URLField(unique=True)
 
+    class Meta:
+        app_label = 'myapp'
+
     def __str__(self):
         return self.name
 
@@ -20,6 +27,9 @@ class Webpage(models.Model):
 class AccessRecord(models.Model):
     name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
     date = models.DateField()
+
+    class Meta:
+        app_label = 'myapp'
 
     def __str__(self):
         return str(self.date)

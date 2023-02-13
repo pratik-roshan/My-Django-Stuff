066074ca96107f2550375eff8d03963ac19f7f10
Pratik Roshan Shah <pratik.roshan.21@gmail.com>
2023-02-13 21:34:03 +0545

    Assigment Updated

--- Assignment/appThree/admin.py
+++ Assignment/appThree/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from appThree.models import User
 # Register your models here.
+admin.site.register(User)
---
+++ Assignment/appThree/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from appThree import views
+
+
+urlpatterns = [
+    path('', views.users, name='users'),
+]

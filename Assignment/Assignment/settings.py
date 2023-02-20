99c40c9f62857e7f0c6eca6e1da05f4cf30199ae
Pratik Roshan Shah <pratik.roshan.21@gmail.com>
2023-02-20 20:25:05 +0545

    LogIn.html Creation

--- Assignment/Assignment/settings.py
+++ Assignment/Assignment/settings.py
@@ -137,7 +137,9 @@ STATICFILES_DIRS = [
 
 # MEDIA 
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
+
+LOGIN_URL = "/appThree/user_login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
